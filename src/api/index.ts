d2e32876871e8791b1a55dc46a18c33aa3e03604
liemhtn <liemhtn@nustechnology.com>
2026-07-30 09:26:12 +0700

    feat: setup cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,11 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
-import { openApiDocument } from '~/api/docs/openapi';
-import { errorHandler } from '~/api/middlewares/error.middleware';
-import { requestLogger } from '~/api/middlewares/request-logger.middleware';
-import routes from '~/api/routes/index';
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import { openApiDocument } from "~/api/docs/openapi";
+import { errorHandler } from "~/api/middlewares/error.middleware";
+import { requestLogger } from "~/api/middlewares/request-logger.middleware";
+import routes from "~/api/routes/index";
+import { corsOptions } from "~/config/cors";
 
 interface ApiInterface {
   server(): Promise<Application>;
@@ -16,26 +17,26 @@ class Api implements ApiInterface {
     app.use(requestLogger);
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    app.get('/api-docs.json', (_req: Request, res: Response) => {
+    app.use(cors(corsOptions));
+    app.get("/api-docs.json", (_req: Request, res: Response) => {
       res.json(openApiDocument);
     });
     app.use(
-      '/api-docs',
+      "/api-docs",
       ...swaggerUi.serve,
       swaggerUi.setup(openApiDocument, {
-        customSiteTitle: 'Express Prisma API Docs',
-      })
+        customSiteTitle: "Express Prisma API Docs",
+      }),
     );
-    app.use('/api/v1', routes);
+    app.use("/api/v1", routes);
 
-    app.get('/health', (_req: Request, res: Response) => {
-      res.json({ status: 'ok', timestamp: new Date().toISOString() });
+    app.get("/health", (_req: Request, res: Response) => {
+      res.json({ status: "ok", timestamp: new Date().toISOString() });
     });
 
-    app.get('/', (_req: Request, res: Response) => {
-      res.send('Welcome to NUS express application!');
+    app.get("/", (_req: Request, res: Response) => {
+      res.send("Welcome to NUS express application!");
     });
-    app.use(cors());
     app.use(errorHandler);
     return app;
   }
---
+++ src/config/cors.ts
@@ -0,0 +1,12 @@
+import { CorsOptions } from "cors";
+import { env } from "./enviroment";
+
+export const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || env.CORS_ORIGIN.split(",").indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS policy: This origin is not allowed by CORS"));
+    }
+  },
+};
